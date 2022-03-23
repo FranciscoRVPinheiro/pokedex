d7f9b35ca075b6eff33da5ff6b94ff733144409f
Ricardo <franciscorvpinheiro49@gmail.com>
2022-03-23 16:57:42 -0300

    add images

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ApiCalls />
 </template>
 
-<style>
-@import './assets/base.css';
+<script setup>
+import ApiCalls from './components/ApiCalls.vue'
+  
+</script>
 
+<style> 
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+body {
+  background-color: #18191A;
+  color: white;
+  margin: 0;
+  padding: 0;
+}
+  
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  font-family: 'Press Start 2P', cursive;
+  text-align: center;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/ApiCalls.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>POKÉDEX</h1>
+  <form @submit.prevent="apiCall">
+    <input v-model="name" type="text" required placeholder="pokemon's name" />
+    <button>GO!</button>
+  </form>
+  
+  <div v-if="noErrMsg">
+    <p>{{ pokemons.name.toUpperCase() }}</p>
+    <img :src="pokemons.sprites.front_default" :alt="pokemons.name" width="200" height="200">
+    <h3>Type</h3>
+      <p><em>{{ pokemons.types[0].type.name }}</em></p>
+    <h3>Ability</h3>
+      <p><em>{{ pokemons.abilities[0].ability.name }}</em></p>
+    <h3>Weight, Height</h3>
+      <p><em>{{ pokemons.weight / 10 }} kg, {{ pokemons.height * 10}} cm</em></p>
+  </div>
+
+  <div class="pokeimage" v-if="!noErrMsg" > 
+    <img src="src/assets/pokeball.png" alt="pokeballs" width="300" height="140" >
+    <p>Try adding the name of a pokémon.</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+let name = ref('')
+let pokemons = ref([])
+let noErrMsg = ref(false)
+
+function apiCall() { axios.get(`https://pokeapi.co/api/v2/pokemon/${name.value.toLowerCase()}`)
+      .then(response => {
+          pokemons.value = response.data
+          noErrMsg.value = true
+      })
+      .catch(error => {
+          console.log(error.response)   
+          noErrMsg.value = false
+      })
+}  
+</script>
+
+<style>
+button {
+  font-family: 'Press Start 2P', cursive;
+  height: 25px;
+  margin: 10px;
+  border-radius: 5px;
+  padding: 3px;
+  background-color: red;
+  color: white;
+}
+
+button:hover {
+  background-color: white;
+  color: red;
+}
+
+input {
+  text-align: center;
+  margin: 5px;
+  border-radius: 5px;
+  width: 150px;
+  height: 25px;
+}  
+
+.pokeimage {
+  margin-top: 50px;
+}
+</style>
